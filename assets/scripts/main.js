40412479f60ae4a400f4cf0a2057bccf9e13e5a8
tlacaba <timlacaba123@gmail.com>
2023-05-21 23:22:33 -0700

    nothing dot

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,8 +53,8 @@ function initializeServiceWorker() {
       try {
         // B3. TODO - Register './sw.js' as a service worker (The MDN article
         //            "Using Service Workers" will help you here)
-        const registration = await navigator.serviceWorker.register("/Lab7_Starter/sw.js", {
-          scope: "/",
+        const registration = await navigator.serviceWorker.register("sw.js", {
+          scope: ".",
         });
         // B4. TODO - Once the service worker has been successfully registered, console
         //            log that it was successful.
